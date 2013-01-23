e902d53422c9e40e52f6f6c7a1651051126988b8
elin <elin@errol.(none)>
2013-01-23 13:56:41 +0100

    First git commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='falk',
+      version=version,
+      description="python course test",
+      long_description="""\
+python course test""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python',
+      author='Elin',
+      author_email='elin.falk_sorqvist@igp.uu.se',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
