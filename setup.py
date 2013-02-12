d2753009cbcaf4d06903378ae09fcb898cddf643
elin <elin@errol.(none)>
2013-02-12 11:51:23 +0100

    Adding requirements

--- setup.py
+++ setup.py
@@ -15,11 +15,12 @@ python course test""",
       url='',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      scripts = ['scripts/getting_data.py'],
+      scripts = ['scripts/getting_data.py', 'scripts/check_repo.py'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+	'untangle',
+	'request'
       ],
       entry_points="""
       # -*- Entry points: -*-
