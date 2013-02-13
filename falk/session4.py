e111bf3f8b95cd88c918e35f667fefca42098d90
elin <elin@errol.(none)>
2013-02-13 12:03:44 +0100

    Added extraction of most common weekday and hour for commits

--- falk/session4.py
+++ falk/session4.py
@@ -4,13 +4,13 @@ from dateutil import parser
 from pandas import Series, DataFrame
 
 def getUserAndPass():
-#    user=raw_input("Username: ")
-    user='elinfalk'
+    '''Ask for username and password'''
+    user=raw_input("Username: ")
     password=getpass.getpass()
     return (user, password)
 
 def fetchCommitInfo(cred):
-
+    '''Get the data from the pythonkurs repository and return it as a dataframe'''
     repositories = requests.get("https://api.github.com/orgs/pythonkurs/repos", auth=cred).json()
     
     repoDict = {} #To save Series in before converting to dataframe
@@ -24,6 +24,7 @@ def fetchCommitInfo(cred):
     return DataFrame(repoDict)
                                              
 def getCommitSeries(commitURL, cred):
+    '''Extract time and message for every commit'''
 
     commits = requests.get(commitURL, auth=cred).json()
     if type(commits) == list: # Didn't find any other better way to make sure there is something to parse
@@ -37,9 +38,25 @@ def getCommitSeries(commitURL, cred):
             messages.append(commitMessage)
         return Series (messages, index=times)
 
+def getCommonWeekdayAndHour(commitDataFrame):
+    '''Find the most common day of the week and the most common hour to commit'''
+    weekDays=['Mon', 'Tue', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'] 
+    commitDates=commitDataFrame.count(1).resample("D", how='sum').fillna(0)
+    commitWeekdayFreq=commitDates.groupby(commitDates.index.weekday).sum()
+    comWeekday = weekDays[commitWeekdayFreq.idxmax()]
+        
+    commitTimes=commitDataFrame.count(1).resample("H", how='sum').fillna(0)
+    commitHourFreq=commitTimes.groupby(commitTimes.index.hour).sum()
+    comHour=commitHourFreq.idxmax()
+    return comWeekday, comHour
+
+    
+def assignment4():
+    cred = getUserAndPass()
+    commitDataFrame=fetchCommitInfo(cred)
+    (day,hour) = getCommonWeekdayAndHour(commitDataFrame)
+    print "Most common weekday is", day, "and the most common hour is", hour
 
 
 if __name__ == '__main__':
-    cred = getUserAndPass()
-    commitDataFrame=fetchCommitInfo(cred)
-
+    assignment4()
