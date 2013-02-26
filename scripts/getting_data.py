2c3c5e4c437d02872a2220f832e28079d0ffc0f7
elin <elin@errol.(none)>
2013-02-26 14:05:06 +0100

    Re-writing to be able to run line_profiler and memory_profiler

--- scripts/getting_data.py
+++ scripts/getting_data.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 from falk.session2 import readNycOutages, calculateRepairFraction
 
-outages = readNycOutages ("http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml")
-print "Fraction of the outages with reason REPAIR:",calculateRepairFraction(outages)
+@profile
+def gettingData():
+    outages = readNycOutages ("http://www.grandcentral.org/developers/data/nyct/nyct_ene.xml")
+    print "Fraction of the outages with reason REPAIR:",calculateRepairFraction(outages)
+
+if __name__ == '__main__':
+    gettingData()
